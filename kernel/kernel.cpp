85882cbba3ee27529f9cbca9f17c88acf7c79c71
Preston Sundar <sundar.preston.789@gmail.com>
2019-11-01 20:27:31 -0500

    Update kernel.cpp

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -2,12 +2,15 @@
  * when compiling this code, tell compiler that there isnt any os to make calls to
  * and that ther is no glibc. this is done in makefile.
  * 
- * 
+ * Use the datatypes universalized in types.h
  * 
  * 
  * 
  * 
  */ 
+
+#include "types.h"
+#include "global_desc_table.h"
 
 void printf(char* str)
 {
@@ -15,7 +18,7 @@ void printf(char* str)
 
 
     // store the pointer to start of video memory
-    unsigned short* video_memory = (unsigned short*) 0xb8000;
+    static uint16_t* video_memory = (uint16_t*) 0xb8000;
 
     for (int i = 0; str[i] != '\0'; ++i)
     {
@@ -27,6 +30,23 @@ void printf(char* str)
 }
 
 
+/* get the pointers to the contructors, filled in by linker 
+ * between the pointers start_ctors and end_ctors lies function pointers 
+ * placed there by the linker, execute these functions. 
+ *
+ */   
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void call_constructors()
+{
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+    {
+        // call the function 
+        (*i)();
+    }
+}
+
 /*   main function of our kernal 
  *   accepts the pointer to multiboot and the magic code (no particular reason to take the magic number)
  * 
@@ -34,12 +54,15 @@ void printf(char* str)
  */
 
 
-extern "C" void kernal_main(void* multiboot_structure, unsigned int magic_number)
+extern "C" void kernel_main(void* multiboot_structure, uint32_t magic_number)
 {
     
     // can't use the standard printf as we're outside an OS currently
     // we don't have access to glibc so we write our own printf
-    printf("Hello, im the os");
+    printf("Hello, WELCOME TO PoopOS's kernel.....so what now?");
+
+    GlobalDescriptorTable gdt;
+
 
     /* kernal never really stops, inf loop */
     while(1);
